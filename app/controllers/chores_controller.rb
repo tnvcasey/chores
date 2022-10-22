edfbfc2d7a14b52aa28fe8b75f591a67fee93e1f
taylorvercillo <taylornvcasey@gmail.com>
2022-10-21 18:01:21 -0600

    chores controller

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -1,2 +1,29 @@
 class ChoresController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+
+    def index
+        chores = Chore.all 
+        render json: chores
+    end 
+
+    def show
+        chore = Chore.find_by(id: params[:id])
+        render json: chore
+    end 
+
+    def create
+        chore = Chore.create(chore_params)
+        render json: chore, status: :created
+    end 
+
+    private 
+
+    def chore_params
+        params.permit(:description, :supplies, :directions)
+    end
+
+    def record_invalid(invalid)
+        render json: { errors: "Validation Errors" }, status: :unprocessable_entity
+    end
+
 end
