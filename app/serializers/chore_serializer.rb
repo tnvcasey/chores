60cc5182e6bf544865fbf014d14e66ced4bf3138
taylorvercillo <taylornvcasey@gmail.com>
2022-10-21 21:12:18 -0600

    added serializer

--- app/serializers/chore_serializer.rb
+++ app/serializers/chore_serializer.rb
@@ -1,4 +1,7 @@
 class ChoreSerializer < ActiveModel::Serializer
-  attributes :id, :description, :details
+  attributes :id, :description, :details, :user_id, :member_id
+
+  belongs_to :user
+  belongs_to :member
 
 end
